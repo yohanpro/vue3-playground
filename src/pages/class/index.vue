81e90d30a45ce650813678ce18034a71776d8186
Yohan Kim <yohan@yohanpro.com>
2022-06-18 10:18:19 +0900

    feat: tailwind 적용

--- src/pages/class/index.vue
+++ src/pages/class/index.vue
@@ -3,11 +3,20 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MyClass",
+  setup() {
+    return {
+      msg: "Hello World",
+    };
+  },
 });
 </script>
 
 <template>
-  <div>MyClass</div>
+  <div>
+    MyClass
+
+    <div class="mt-4">{{ msg }}</div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
